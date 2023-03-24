3840e5c9c62a5393e8e2edf9b88fc7c42e24071e
João Rodrigues Parra <j-r-p@outlook.com>
2023-03-24 13:00:17 -0300

    versão final

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ console.log (`
 Feito com dedicação por JoaoLive100 (João Rodrigues Parra) para o processo seletivo da DTI Digital.
 `);
 
+deleteReminder('','','');
 });
 
 const form = document.getElementById('form');
@@ -17,12 +18,19 @@ form.addEventListener('submit', e => {
 
     let reminderName = $('#name').val();
     let reminderDate = $('#date').val();
-    insertReminder(reminderDate, reminderName);
-});
+    let data = new Date();
+    let dia = String(data.getDate()).padStart(2, '0');
+    let mes = String(data.getMonth() + 1).padStart(2, '0');
+    let ano = data.getFullYear();
+    let dataAtual = ano+'-'+mes+'-'+dia;
 
-function deleteReminder (element) {
-    element.innerHTML = '';
-}
+    if (reminderDate < dataAtual) {
+        alert("A data não pode ser anterior a data atual!");
+    }
+    else {
+        insertReminder(reminderDate, reminderName);
+    }
+});
 
 function insertReminder(reminderDate, reminderName) {
 
@@ -37,75 +45,108 @@ function insertReminder(reminderDate, reminderName) {
         },
         success: function(response) {
             var jsonData = response;
-            console.log(jsonData);
-
             var ul = document.getElementById("main-ul");
             ul.innerHTML = '';
-
             for (var data in jsonData) {
                 if (jsonData.hasOwnProperty(data)) {
-
                     var liData = document.createElement("li");
                     liData.innerHTML = data;
-
-
                     ul.appendChild(liData);
-
-
                     var ulTarefas = document.createElement("ul");
-
-
                     var tarefas = jsonData[data];
                     for (var i = 0; i < tarefas.length; i++) {
                         var liTarefa = document.createElement("li");
                         liTarefa.style.display = "inline-block";
                         liTarefa.style.width = "100%";
-
                         var img = document.createElement("img");
                         img.style.display = "inline-block";
                         img.style.width = "15px";
                         img.style.height = "15px";
                         img.style.verticalAlign = "middle";
-                        
                         img.src = "../img/remove.png";
                         img.classList.add("deleteicon");
                         liTarefa.appendChild(img);
-
                         var span = document.createElement("span");
                         span.style.display = "inline-block";
                         span.style.marginLeft = "10px";
                         span.style.verticalAlign = "middle";
                         span.innerHTML = tarefas[i];
                         liTarefa.appendChild(span);
-
                         ulTarefas.appendChild(liTarefa);
                     }
-
                     liData.appendChild(ulTarefas);
                 }
             }
-            const elements = document.querySelectorAll('.deleteicon');
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].addEventListener('click', function() {
-                    const li = elements[i].closest('li');
-                    const date = li.parentNode.previousSibling.innerHTML;
-                    const tarefa = li.lastChild.innerHTML;
-                    $.ajax({
-                        url: './remindme-api/index.php',
-                        type: 'post',
-                        dataType: 'json',
-                        data: {
-                            date : date,
-                            tarefa : tarefa,
-                            delete : "true"
-                        },
-                        success: function(response) {
-                            console.log(response);
-                            li.parentNode.removeChild(li);
-                        }
-                    });
-                });
+            insertEventListener();
+        }
+    });
+}
+
+function insertEventListener() {
+    const elements = document.querySelectorAll('.deleteicon');
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', function() {
+            const li = elements[i].closest('li');
+            const string = li.parentNode.parentNode.innerHTML;
+            const regex = /\d{4}-\d{2}-\d{2}/;
+            const match = string.match(regex);
+            const date = match[0];
+            const tarefa = li.lastChild.innerHTML;
+            deleteReminder(li, date, tarefa);
+        });
+    }
+}
+
+function deleteReminder (li,dataB, tarefaB) {
+
+    $.ajax({
+        url: './remindme-api/index.php',
+        type: 'post',
+        dataType: 'json',
+        data: {
+            dataB : dataB,
+            tarefaB : tarefaB,
+            deleteB : "true"
+        },
+        success: function(responseB) {
+            if(li) {
+                li.parentNode.removeChild(li);
             }
+            var jsonData = responseB;
+            console.log(jsonData);
+            var ul = document.getElementById("main-ul");
+            ul.innerHTML = '';
+            for (var data in jsonData) {
+                if (jsonData.hasOwnProperty(data)) {
+                    var liData = document.createElement("li");
+                    liData.innerHTML = data;
+                    ul.appendChild(liData);
+                    var ulTarefas = document.createElement("ul");
+                    var tarefas = jsonData[data];
+                    for (var i = 0; i < tarefas.length; i++) {
+                        var liTarefa = document.createElement("li");
+                        liTarefa.style.display = "inline-block";
+                        liTarefa.style.width = "100%";
+                        var img = document.createElement("img");
+                        img.style.display = "inline-block";
+                        img.style.width = "15px";
+                        img.style.height = "15px";
+                        img.style.verticalAlign = "middle";
+                        img.src = "../img/remove.png";
+                        img.classList.add("deleteicon");
+                        liTarefa.appendChild(img);
+                        var span = document.createElement("span");
+                        span.style.display = "inline-block";
+                        span.style.marginLeft = "10px";
+                        span.style.verticalAlign = "middle";
+                        span.innerHTML = tarefas[i];
+                        liTarefa.appendChild(span);
+                        ulTarefas.appendChild(liTarefa);
+                    }
+                    liData.appendChild(ulTarefas);
+                }
+            }
+            insertEventListener();
         }
     });
 }
--- remindme-api/index.php
+++ remindme-api/index.php
@@ -4,39 +4,59 @@
         $date = $_POST['date'];
         $delete = $_POST['delete'];
 
-        if($delete == "false") {
-            $connection = new mysqli('localhost','root','p@ssw0rd013459','remindme_prd');
-        
-            $sql = "CALL sp_insert_tb_cad_reminder ('$name', '$date')";
-            $connection->set_charset('utf8');
+        $connection = new mysqli('localhost','root','p@ssw0rd013459','remindme_prd');
     
-            $result = $connection->query($sql);
-            
-            $grouped_results = array();
-    
-            while ($row = $result->fetch_assoc()) {
-                $date = $row['pDate'];
-                $name = $row['pName'];
-    
-                if (!isset($grouped_results[$date])) {
-                    $grouped_results[$date] = array();
-                }
-                $grouped_results[$date][] = $name;
-            }
+        $sql = "CALL sp_insert_tb_cad_reminder ('$name', '$date')";
+        $connection->set_charset('utf8');
 
-            $json = json_encode($grouped_results);
-            echo $json;
+        $result = $connection->query($sql);
+        
+        $grouped_results = array();
+
+        while ($row = $result->fetch_assoc()) {
+            $date = $row['pDate'];
+            $name = $row['pName'];
+
+            if (!isset($grouped_results[$date])) {
+                $grouped_results[$date] = array();
+            }
+            $grouped_results[$date][] = $name;
+        }
+
+        $json = json_encode($grouped_results);
+        echo $json;
+    }
+    if(isset($_POST['tarefaB']) && isset($_POST['dataB']) && isset($_POST['deleteB'])) {
+        $name = $_POST['tarefaB'];
+        $date = $_POST['dataB'];
+        $delete = $_POST['deleteB'];
+
+        $connectionB = new mysqli('localhost','root','p@ssw0rd013459','remindme_prd');
+        
+        if ($date === '') {
+            $sqlB = "CALL sp_delete_tb_cad_reminder ('$name', null)";
         }
         else {
-            $name = $_POST['name'];
-            $date = $_POST['date'];
-    
-            $connection = new mysqli('localhost','root','p@ssw0rd013459','remindme_prd');
-    
-            $sql = "CALL sp_delete_tb_cad_reminder ('$name', '$date')";
-            $connection->set_charset('utf8');
-    
-            $result = $connection->query($sql);
+            $sqlB = "CALL sp_delete_tb_cad_reminder ('$name', '$date')";
         }
+        
+        $connectionB->set_charset('utf8');
+
+        $resultB = $connectionB->query($sqlB);
+
+        $grouped_resultsB = array();
+
+        while ($rowB = $resultB->fetch_assoc()) {
+            $dateB = $rowB['pDate'];
+            $nameB = $rowB['pName'];
+
+            if (!isset($grouped_resultsB[$dateB])) {
+                $grouped_resultsB[$dateB] = array();
+            }
+            $grouped_resultsB[$dateB][] = $nameB;
+        }
+
+        $jsonB = json_encode($grouped_resultsB);
+        echo $jsonB;
     }
 ?>
